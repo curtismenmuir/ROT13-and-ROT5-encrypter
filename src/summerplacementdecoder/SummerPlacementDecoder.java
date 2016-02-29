3425dec45448926db6e88914903dae2da062f1be
Curtis Menmuir <curtismenmuir@gmail.com>
2016-02-29 20:15:43 +0000

    initial commit of code
    Just realised I never committed the code!

---
+++ src/summerplacementdecoder/SummerPlacementDecoder.java
@@ -0,0 +1,24 @@
+/*
+ * This application will encrypt/decrypt a message using ROT13 and ROT5. 
+ * This class will only start the User Interface.
+ *
+ * Author: Curtis Menmuir
+ * Date: 1/5/2015
+ */
+package summerplacementdecoder;
+
+/**
+ *
+ * @author: Curtis Menmuir
+ */
+public class SummerPlacementDecoder {
+
+    /**
+     * Launches User Interface
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SummerPlacementDecoderUI.main(args);
+    }
+}
